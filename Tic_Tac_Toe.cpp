f1312ee66a1403a95148859980d1183d08f6e255
Daniel Smith <dsmit017@ucr.edu>
2018-08-31 15:19:09 -0700

    Added checkWin function

--- Tic_Tac_Toe.cpp
+++ Tic_Tac_Toe.cpp
@@ -3,6 +3,17 @@ using namespace std;
 
 char board[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
+int checkWin();
+void printBoard();
+
+int main()
+{
+	
+	printBoard();
+	
+	return 0;
+}
+
 void printBoard()
 {
 	cout << "\n\tTic Tac Toe\n\n";
@@ -27,10 +38,42 @@ void printBoard()
 	return;
 }
 
-int main()
+bool checkWin()
 {
-	
-	printBoard();
-	
-	return 0;
+	if(board[1] == board[2] && board[2] == board[3])
+	{
+		return true;
+	}
+	else if(board[4] == board[5] && board[5] == board[6])
+	{
+		return true;
+	}
+	else if(board[7] == board[8] && board[8] == board[9])
+	{
+		return true;
+	}
+	else if(board[1] == board[4] && board[4] == board[7])
+	{
+		return true;
+	}
+	else if(board[2] == board[5] && board[5] == board[8])
+	{
+		return true;
+	}
+	else if(board[3] == board[6] && board[6] == board[9])
+	{
+		return true;
+	}
+	else if(board[1] == board[5] && board[5] == board[9])
+	{
+		return true;
+	}
+	else if(board[3] == board[5] && board[5] == board[7])
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
